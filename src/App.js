4fe012b9c8ff3b63491f99cff12e760ce4cdf6e5
Abdulkareem Mustapha <aphatheology@gmail.com>
2020-08-27 12:28:16 +0100

    Update the neccessary files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+import Nav from './Nav'
+import Form from './Form';
+import Footer from './Footer'
+
+import {} from 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+    return(
+        <div>
+            
+                <Nav />
+                <Form />                
+                <Footer />
+            
+            
+        </div>
+    )
+}
+
+export default App
